8fdfd09f9d8476752f499b2ad2810a9070263120
9RITUSAM <samdariya.ritu@gmail.com>
2025-01-25 15:16:11 +0530

    .

--- src/app/Components/ChatComponent.js
+++ src/app/Components/ChatComponent.js
@@ -2,9 +2,9 @@
 
 import Chatbot from "react-chatbot-kit";
 import 'react-chatbot-kit/build/main.css';
-import ActionProvider from "../actionprovider";
 import config from "../config";
 import MessageParser from "../MessageParser";
+import ActionProvider from "../Actionprovider";
 
 const ChatComponent = () => {
     return (
